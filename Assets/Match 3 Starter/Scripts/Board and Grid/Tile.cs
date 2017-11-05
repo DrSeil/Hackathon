8bfa742c123ad8a12ed657df84ccbdb161d0e428
Michael Royston <michael@royston.com>
2017-11-04 18:19:08 -0700

    Back to where I was before Deletion
    And we're back.  Working line draw

--- Assets/Match 3 Starter/Scripts/Board and Grid/Tile.cs
+++ Assets/Match 3 Starter/Scripts/Board and Grid/Tile.cs
@@ -25,18 +25,28 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Tile : MonoBehaviour {
-	private static Color selectedColor = new Color(.5f, .5f, .5f, 1.0f);
-	private static Tile previousSelected = null;
-
+    private static Color selectedColor = new Color(.5f, .5f, .5f, 1.0f);
+    private static Tile previousSelected = null;
+    private static List<GameObject> selectedPath = new List<GameObject>();
 	private SpriteRenderer render;
 	private bool isSelected = false;
+    private static bool mousedown;
 
 	private Vector2[] adjacentDirections = new Vector2[] { Vector2.up, Vector2.down, Vector2.left, Vector2.right };
+    private Vector2[] adjacentDirectionsVert = new Vector2[] { Vector2.up, Vector2.zero, Vector2.down};
+    private Vector2[] adjacentDirectionsHorz = new Vector2[] { Vector2.left, Vector2.zero, Vector2.right};
 
-	void Awake() {
+
+
+    void Awake() {
 		render = GetComponent<SpriteRenderer>();
     }
-
+    private void StartPath()
+    {
+        selectedPath.Clear();
+        selectedPath.Add(gameObject);
+        Select();
+    }
 	private void Select() {
 		isSelected = true;
 		render.color = selectedColor;
@@ -49,5 +59,75 @@ public class Tile : MonoBehaviour {
 		render.color = Color.white;
 		previousSelected = null;
 	}
+    private void OnMouseDown()
+    {
+        if (render.sprite == null || BoardManager.instance.IsShifting)
+        {
+            return;
+        }
+        mousedown = true;
+        StartPath();
+    }
+    private void OnMouseEnter()
+    {
+        if(mousedown)
+        {
+            if (isAdjacent() && previousSelected.render.sprite == render.sprite)
+            {
+                selectedPath.Add(gameObject);
+                Select();
+            }
+        }
+    }
+    private void OnMouseUp()
+    {
+        mousedown = false;
+        for(int i=selectedPath.Count-1; i>=0;i--)
+        {
+            selectedPath[i].GetComponent<Tile>().Deselect();
+            selectedPath.Remove(selectedPath[i]);
+        }
+    }
+    private bool isAdjacent()
+    {
+        GameObject tempVert;
+        GameObject temp;
+        for(int i =0; i<adjacentDirectionsVert.Length;i++)
+        {
+            if(adjacentDirectionsVert[i]!=Vector2.zero)
+            {
+                tempVert = GetAdjacent(adjacentDirectionsVert[i]);
+            }
+            else
+            {
+                tempVert = gameObject;
+            }
+            for(int j=0;j<adjacentDirectionsHorz.Length;j++)
+            {
+                if(adjacentDirectionsHorz[j]!=Vector2.zero&&tempVert)
+                {
+                    temp = tempVert.GetComponent<Tile>().GetAdjacent(adjacentDirectionsHorz[j]);
+                }
+                else
+                {
+                    temp = tempVert;
+                }
+                if(previousSelected.gameObject == temp)
+                {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    private GameObject GetAdjacent(Vector2 castDir)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, castDir);
+        if (hit.collider != null)
+        {
+            return hit.collider.gameObject;
+        }
+        return null;
+    }
 
 }
